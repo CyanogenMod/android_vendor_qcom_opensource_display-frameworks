09da768794c8c6acd786d73239ed5a739695b8cb
Raj Kamal <rkamal@codeaurora.org>
2015-07-20 10:46:27 +0530

    display-frameworks: Extend the classes defined in SF
    Add support for custom classes ExSurfaceFlinger,
    ExLayer, ExHWComposer and ExVirtualDisplaySurface
    
    Change-Id: Iab8c4835faaad9b436bbd34d38702741eb0cf12d

---
+++ native/services/surfaceflinger/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CLANG := true
+
+LOCAL_MODULE                    := libexsurfaceflinger
+LOCAL_MODULE_TAGS               := optional
+LOCAL_MODULE_OWNER              := qti
+
+LOCAL_PROPRIETARY_MODULE        := false
+LOCAL_CFLAGS                    := -DLOG_TAG=\"ExSurfaceFlinger\"
+LOCAL_CFLAGS                    += -DHAS_CONTEXT_PRIORITY
+
+LOCAL_C_INCLUDES                += $(TARGET_OUT_HEADERS)
+LOCAL_C_INCLUDES                += hardware/qcom/display/libgralloc \
+                                   hardware/qcom/display/libqdutils \
+                                   frameworks/native/services/surfaceflinger
+
+LOCAL_SHARED_LIBRARIES          := libsurfaceflinger libui libgui libqdutils \
+                                   libbinder libutils libcutils \
+                                   libandroid
+
+LOCAL_SRC_FILES                 := ExLayer.cpp \
+                                   ExSurfaceFlinger.cpp \
+                                   DisplayHardware/ExVirtualDisplaySurface.cpp \
+                                   DisplayHardware/ExHWComposer.cpp
+
+include $(BUILD_STATIC_LIBRARY)
